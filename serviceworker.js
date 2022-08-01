05d5790f899b23bd91c1c1783cbedaf1056d7ade
an war <long369486562@hotmail.com>
2022-08-01 15:16:42 +0800

    modified:   serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -6,49 +6,58 @@ const __DEBUG__ = false;
 //配置两个离线用资源
 const offlineResources = ['/', '/offline.html', '/offline.svg'];
 const ignoreFetch = [
-//忽略抓取的URL或目录，请酌情修改
-/https?:\/\/xiongzhang.baidu.com\//, 
-/https?:\/\/ae.bdstatic.com\//, 
-/https?:\/\/msite.baidu.com\//, 
-/https?:\/\/s.bdstatic.com\//, 
-/https?:\/\/timg01.bdimg.com\//, 
-/https?:\/\/zz.bdstatic.com\//, 
-/https?:\/\/hm.baidu.com\//, 
-/https?:\/\/jspassport.ssl.qhimg.com\//, 
-/https?:\/\/s.ssl.qhres.com\//, 
-/https?:\/\/changyan.itc.cn\//, 
-/https?:\/\/changyan.sohu.com\//, 
-/.php$/, 
-/more/, 
+  //忽略抓取的URL或目录，请酌情修改
+  /https?:\/\/xiongzhang.baidu.com\//,
+  /https?:\/\/ae.bdstatic.com\//,
+  /https?:\/\/msite.baidu.com\//,
+  /https?:\/\/s.bdstatic.com\//,
+  /https?:\/\/timg01.bdimg.com\//,
+  /https?:\/\/zz.bdstatic.com\//,
+  /https?:\/\/hm.baidu.com\//,
+  /https?:\/\/jspassport.ssl.qhimg.com\//,
+  /https?:\/\/s.ssl.qhres.com\//,
+  /https?:\/\/changyan.itc.cn\//,
+  /https?:\/\/changyan.sohu.com\//,
+  /.php$/,
+  /more/,
 ];
 //开始安装
 function onInstall(event) {
   log('install event in progress.');
   event.waitUntil(updateStaticCache());
 }
+//处理cache字段
+function cacheKey() { return [version, ...arguments].join(':'); }
 //更新静态缓存
 function updateStaticCache() {
   return caches.open(cacheKey('offline'))
-         .then((cache) => {
-          return cache.addAll(offlineResources);
-         })
-         .then(() => {
-          log('installation complete!');
-         }); 
+    .then((cache) => {
+      return cache.addAll(offlineResources);
+    })
+    .then(() => {
+      log('installation complete!');
+    });
 }
+//更新动态缓存
 function onFetch(event) { const request = event.request; if (shouldAlwaysFetch(request)) { event.respondWith(networkedOrOffline(request)); return; } if (shouldFetchAndCache(request)) { event.respondWith(networkedOrCached(request)); return; } event.respondWith(cachedOrNetworked(request)); }
 function networkedOrCached(request) { return networkedAndCache(request).catch(() => { return cachedOrOffline(request) }); }
-function networkedAndCache(request) { return fetch(request).then((response) => { var copy = response.clone();
-        caches.open(cacheKey('resources')).then((cache) => { cache.put(request, copy); });
-        log("(network: cache write)", request.method, request.url); return response; }); }
+function networkedAndCache(request) {
+  return fetch(request).then((response) => {
+    var copy = response.clone();
+    caches.open(cacheKey('resources')).then((cache) => { cache.put(request, copy); });
+    log("(network: cache write)", request.method, request.url); return response;
+  });
+}
 function cachedOrNetworked(request) { return caches.match(request).then((response) => { log(response ? '(cached)' : '(network: cache miss)', request.method, request.url); return response || networkedAndCache(request).catch(() => { return offlineResponse(request) }); }); }
 function networkedOrOffline(request) { return fetch(request).then((response) => { log('(network)', request.method, request.url); return response; }).catch(() => { return offlineResponse(request); }); }
 function cachedOrOffline(request) { return caches.match(request).then((response) => { return response || offlineResponse(request); }); }
 function offlineResponse(request) { log('(offline)', request.method, request.url); if (request.url.match(/\.(jpg|png|gif|svg|jpeg)(\?.*)?$/)) { return caches.match('/offline.svg'); } else { return caches.match('/offline.html'); } }
-function onActivate(event) { log('activate event in progress.');
- event.waitUntil(removeOldCache()); }
+function onActivate(event) {
+  log('activate event in progress.');
+  event.waitUntil(removeOldCache());
+}
 function removeOldCache() { return caches.keys().then((keys) => { return Promise.all(keys.filter((key) => { return !key.startsWith(version); }).map((key) => { return caches.delete(key); })); }).then(() => { log('removeOldCache completed.'); }); }
-function cacheKey() { return [version, ...arguments].join(':'); }
+
 function log() { if (developmentMode()) { console.log("SW:", ...arguments); } }
 function shouldAlwaysFetch(request) { return __DEVELOPMENT__ || request.method !== 'GET' || ignoreFetch.some(regex => request.url.match(regex)); }
 function shouldFetchAndCache(request) { return ~request.headers.get('Accept').indexOf('text/html'); }
