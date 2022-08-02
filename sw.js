216dd5182e8c0899b681d957f467dfd3111eb4c3
an war <long369486562@hotmail.com>
2022-08-02 17:50:22 +0800

    modified:   main.js 	modified:   sw.js

--- sw.js
+++ sw.js
@@ -22,141 +22,207 @@ const ignoreFetch = [
   /.php$/,
   /more/,
 ];
-//自定义打印方法
-function log() {
-  // if (developmentMode()) {
-  console.log("SW:", ...arguments);
-  // }
-}
-//开始安装
-function onInstall(event) {
-  log('install event in progress.');
-  event.waitUntil(updateStaticCache());
-}
-//处理cache字段
-function cacheKey() { return [version, ...arguments].join(':'); }
-//更新静态缓存
-function updateStaticCache() {
-  return caches.open(cacheKey('offline'))
-    .then((Cache) => {
-      console.log(Cache);
-      //接受一个 URL 数组，检索它们，并将生成的 response 对象添加到给定的缓存中
-      return Cache.addAll(offlineResources);
-    })
-    .then(() => {
-      log('installation complete!');
-    });
-}
-//更新动态缓存--指各种资源
-function onFetch(event) {
-  //返回各种资源加载的request对象
-  const request = event.request;
-  //根据 当前环境，请求方法，url判断是否更新缓存
-  if (shouldAlwaysFetch(request)) {
-    event.respondWith(networkedOrOffline(request));
-    return;
-  }
-   //根据 请求头  header 接受的返回类型  判断是否更新缓存
-  if (shouldFetchAndCache(request)) {
-    event.respondWith(networkedOrCached(request));
-    return;
-  }
-  //其他未指定的缓存
-  event.respondWith(cachedOrNetworked(request));
-}
-//判断是否有页面缓存
-function networkedOrCached(request) {
-  return networkedAndCache(request)
-    .catch(() => {
-      return cachedOrOffline(request)
-    });
-}
-//处理未指定的缓存
-function networkedAndCache(request) {
-  return fetch(request).then((response) => {
-    var copy = response.clone();
-    caches.open(cacheKey('resources')).then((cache) => { cache.put(request, copy); });
-    log("(network: cache write)", request.method, request.url); return response;
-  });
-}
-//判断未指定的资源是否有缓存
-function cachedOrNetworked(request) {
-  return caches.match(request)
-    .then((response) => {
-      log(response ? '(cached)' : '(network: cache miss)', request.method, request.url);
-      //如果没有缓存开始创建缓存并返回
-      return response || networkedAndCache(request).catch(() => {
-          return offlineResponse(request)
-        });
-    });
-}
-//判断资源是否已经缓存，如果已经缓存，直接从缓存中返回资源，否则重新请求--资源
-function networkedOrOffline(request) {
-  return fetch(request)
-    .then((response) => {
-      log('(network)', request.method, request.url);
-      return response;
-    })
-    .catch(() => {
-      return offlineResponse(request);
-    });
-}
-//判断资源是否已经缓存，如果已经缓存，直接从缓存中返回资源，否则重新请求--页面
-function cachedOrOffline(request) {
-  return caches.match(request)
-    .then((response) => {
-      return response || offlineResponse(request);
-    });
-}
-//从缓存中取出已经缓存的静态资源，直接返回给请求--公共
-function offlineResponse(request) {
-  log('(offline)', request.method, request.url);
-  if (request.url.match(/\.(jpg|png|gif|svg|jpeg)(\?.*)?$/)) {
-    return caches.match('/offline.svg');
-  } else {
-    return caches.match('/offline.html');
-  }
-}
-function onActivate(event) {
-  log('activate event in progress.');
-  event.waitUntil(removeOldCache());
-}
-function removeOldCache() {
-  return caches.keys()
-    .then((keys) => {
-      return Promise.all(keys.filter((key) => {
-        return !key.startsWith(version);
+//添加 `install` 事件监听器
+self.addEventListener('install', (event) => {
+  console.info('Event: Install');
+
+  event.waitUntil(
+    caches.open(cacheName)
+    .then((cache) => {
+      //[] 要缓存的文件 & 如果任何文件不存在 `addAll` 将失败
+      return cache.addAll(files)
+      .then(() => {
+        console.info('All files are cached');
+        return self.skipWaiting(); //强制等待的服务工作者成为活跃的服务工作者
       })
-        .map((key) => {
-          return caches.delete(key);
-        }));
+      .catch((error) =>  {
+        console.error('Failed to cache', error);
+      })
     })
-    .then(() => {
-      log('removeOldCache completed.');
+  );
+});
+
+/*
+  FETCH EVENT：在安装后由索引页面发出的每个请求触发。
+*/
+self.addEventListener('click', (event) => {
+  console.info('Event: Install');
+
+  Notification.requestPermission().then(function(result) {
+    // result可能是是granted, denied, 或default.
+  });
+});
+
+//添加 `fetch` 事件监听器
+self.addEventListener('fetch', (event) => {
+  console.info('Event: Fetch');
+
+  var request = event.request;
+  var url = new URL(request.url);
+  if (url.origin === location.origin) {
+    // 静态文件缓存
+    event.respondWith(cacheFirst(request));
+  } else {
+    // 动态 API 缓存
+    event.respondWith(networkFirst(request));
+  }
+
+  // //检查导航预加载响应
+  // if (event.preloadResponse) {
+  //   console.info('使用导航预加载');
+  //   return response;
+  // }
+});
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+  const dynamicCache = await caches.open(cacheName);
+  try {
+    const networkResponse = await fetch(request);
+    // Cache the dynamic API response
+    dynamicCache.put(request, networkResponse.clone()).catch((err) => {
+      console.warn(request.url + ': ' + err.message);
     });
+    return networkResponse;
+  } catch (err) {
+    const cachedResponse = await dynamicCache.match(request);
+    return cachedResponse;
+  }
 }
-//判断是否忽略资源缓存
-function shouldAlwaysFetch(request) {
-  //ignoreFetch是一个忽略元素的regex正则列表
-  return __DEVELOPMENT__ || request.method !== 'GET' || ignoreFetch.some(regex => request.url.match(regex));
-}
-//判断是否忽略资源缓存--接受类型
-function shouldFetchAndCache(request) {
-  return ~request.headers.get('Accept').indexOf('text/html');
-}
-function developmentMode() {
-  return __DEVELOPMENT__ || __DEBUG__;
-}
-log("Hello from ServiceWorker land!", version);
-//安装PWA缓存开始---------
-//默认传递一个event事件对象
-//创建安装
-self.addEventListener('install', onInstall);
-//进行资源缓存-和取出缓存
-self.addEventListener('fetch', onFetch);
-//删除版本不一致的缓存
-self.addEventListener("activate", onActivate);
-//安装PWA缓存结束--------
+
+/*
+  ACTIVATE EVENT：注册后触发一次，也用于清理缓存。
+*/
+//添加 `activate` 事件监听器
+self.addEventListener('activate', (event) => {
+  console.info('Event: Activate');
+
+  //导航预加载帮助我们在服务工作者启动时发出并行请求。
+//启用 -chrome://flags/#enable-service-worker-navigation-preload
+//支持 -Chrome 57 beta（作为旗帜）
+//更多信息 -https://developers.google.com/web/updates/2017/02/navigation-preload#the-problem
+
+  // 检查是否支持 navigationPreload
+  // if (self.registration.navigationPreload) { 
+  //   self.registration.navigationPreload.enable();
+  // }
+  // else if (!self.registration.navigationPreload) { 
+  //   console.info('您的浏览器不支持导航预加载。');
+  // }
+
+  //删除旧的和不需要的缓存
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache); //删除旧缓存（缓存 v1）
+          }
+        })
+      );
+    })
+    .then(function () {
+      console.info("Old caches are cleared!");
+      // 告诉服务人员激活当前的
+// 而不是等待旧的完成。
+      return self.clients.claim(); 
+    }) 
+  );
+});
+
+/*
+  PUSH EVENT：每次收到推送通知时触发。
+*/
+//添加 `push` 事件监听器
+self.addEventListener('push', (event) => {
+  console.info('Event: Push');
+
+  var title = 'Push notification demo';
+  var body = {
+    'body': 'click to return to application',
+    'tag': 'demo',
+    'icon': './images/icons/apple-touch-icon.png',
+    'badge': './images/icons/apple-touch-icon.png',
+    //自定义操作按钮
+    'actions': [
+      { 'action': 'yes', 'title': 'I ♥ this app!'},
+      { 'action': 'no', 'title': 'I don\'t like this app'}
+    ]
+  };
+
+  event.waitUntil(self.registration.showNotification(title, body));
+});
+
+/*
+  背景同步事件：在 `bg sync` 注册并且页面有网络连接后触发。
+  它将尝试获取 github 用户名，如果满足则同步完成。如果失败了，
+  另一个同步计划重试（也将等待网络连接）
+*/
+
+self.addEventListener('sync', (event) => {
+  console.info('Event: Sync');
+
+  //从 devTools 检查注册的同步名称或模拟同步
+  if (event.tag === 'github' || event.tag === 'test-tag-from-devtools') {
+    event.waitUntil(
+      //检查所有打开的选项卡并将 postMessage 发送到这些选项卡
+      self.clients.matchAll().then((all) => {
+        return all.map((client) => {
+          return client.postMessage('online'); //要发出获取请求，请检查 app.js -行号：122
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+    );
+  }
+});
+
+/*
+  通知事件：当用户点击通知时触发。
+*/
+//添加“通知”点击事件监听器
+self.addEventListener('notificationclick', (event) => {
+  var url = 'https://demopwa.in/';
+
+  //在推送通知中收听自定义操作按钮
+  if (event.action === 'yes') {
+    console.log('I ♥ this app!');
+  }
+  else if (event.action === 'no') {
+    console.warn('I don\'t like this app');
+  }
+
+  event.notification.close(); //关闭通知
+
+  //点击通知后打开应用
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window'
+    })
+    .then((clients) => {
+      for (var i = 0; i < clients.length; i++) {
+        var client = clients[i];
+        //如果站点已打开，请关注站点
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+
+      //如果网站无法打开，请在新窗口中打开
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  );
+});
 //手动唤醒浏览器询问用户是否安装到桌面
 self.addEventListener('beforeinstallprompt', function(event) { // 监听到可安装事件，进行触发提醒用户
   setTimeout(function() {
