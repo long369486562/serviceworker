bdcace5e0ac9be07327cd57e67ac0acd009c12d4
an war <long369486562@hotmail.com>
2022-08-02 17:15:50 +0800

    modified:   main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function () {
     window.addEventListener('load', function () {
         if ('serviceWorker' in navigator) {
-          navigator.serviceWorker.register("/apps/js/sw.js").then(function (registration) {
+          navigator.serviceWorker.register("./sw.js").then(function (registration) {
             console.log('Service Worker Registered,register script: serviceworker.js.');
           }).catch(function (error) {
             // registration failed
