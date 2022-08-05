33e1275166d12e60a0d4a2295c5670cdee8ef219
an war <long369486562@hotmail.com>
2022-08-05 11:38:18 +0800

    modified:   main.js 	new file:   manifest.json

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
         import { Workbox } from 'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-window.prod.mjs';
 
         if ('serviceWorker' in navigator) {
-            const wb = new Workbox('/sw.js');
+            const wb = new Workbox('/apps/js/sw.js');
             wb.register();
         }
     })
