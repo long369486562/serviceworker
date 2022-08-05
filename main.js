336e9d68fc1f5e4e06c65e3944c29617eb08d6b5
long369486562 <37871356+long369486562@users.noreply.github.com>
2022-08-05 14:20:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function () {
     window.addEventListener('load', function () {
         importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-window.prod.mjs");
-
+asdfasdfasdfasdf
         if ('serviceWorker' in navigator) {
             const wb = new Workbox('/apps/js/sw.js');
             wb.register();
